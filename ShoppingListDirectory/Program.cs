ee06dddaa7aead7652f43687065cf2041b58ff07
Simon S <ssuvemaa@tktk.ee>
2022-03-18 10:19:12 +0200

    add new projects

---
+++ ShoppingListDirectory/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace ShoppingListDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //programm võimaldab luua kaustajal ise kausta ja faili;
+            //jätan kasutajale võimaluse neid ise nimetada;
+            //kasutaja võiks nimeks määrata: ShoppingList kausta ja seejärel myShoppingList.txt faili; 
+
+
+
+            string rootDirectory = @"C:\Users\...\samples";
+            Console.WriteLine("Enter directory name:");
+
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Enter file name:");
+            string fileName = Console.ReadLine();
+
+
+            if (Directory.Exists($"{rootDirectory}\\{newDirectory}") && File.Exists($"{rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory and File exist.");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+            }
+        }
+    }
+}
---
+++ myShoppingList/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace myShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Näiteks soovid: apple, orange, watermelon, strawberry, blueberry
+            string fileLocation = @"C:\Users\...\samples\ShoppingList";
+            string fileName = @"\\myShoppingList.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myWishList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add a product to your shopping list? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter product:");
+                    string userWish = Console.ReadLine();
+                    myWishList.Add(userWish);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Take care!");
+                }
+
+            }
+
+
+            Console.Clear();
+
+            foreach (string wish in myWishList)
+            {
+                Console.WriteLine($"Your product: {wish}");
+            }
+
+
+            File.WriteAllLines($"{fileLocation}{fileName}", myWishList);
+        }
+    }
+}
